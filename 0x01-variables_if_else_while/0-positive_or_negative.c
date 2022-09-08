af2e70681568c1fd8ed42244ffe573777559b54a
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-09 00:42:44 +0200

    commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n > 0){
+		printf("%i is positive", n);
+	}else if(n < 0){
+		printf("%i is negative", n);
+	}else{
+		printf("%i is zero", n);
+	}
+	return (0);
+}
