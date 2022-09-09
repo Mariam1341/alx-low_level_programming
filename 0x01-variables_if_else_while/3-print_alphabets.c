861fd66331421ba89df2bfde7d5301b2ce455590
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-10 01:46:12 +0200

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,10 @@
-#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description:  prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line.
+ * Return: 0
+ */
 int main(void)
 {
 	int num = 97;
