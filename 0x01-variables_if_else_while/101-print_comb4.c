ce5e44f9cc6ebd7e44ed99f31e8364939514e822
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-09 22:00:32 +0200

    readme

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,12 +8,11 @@ int main(void)
 		n1 = num %10;
 		n2 = num /10;
 		n3 = num /100;
-
 		if(n3 < n2 && n3 < n1){	
 			if(n2 < n1){
-				putchar(n3%10+'00');
-				putchar(n2%10+'00');
-				putchar(n1%10+'00');
+				putchar(n3%10+'0');
+				putchar(n2%10+'0');
+				putchar(n1%10+'0');
 				putchar(',');
 				putchar(' ');
 			}					
