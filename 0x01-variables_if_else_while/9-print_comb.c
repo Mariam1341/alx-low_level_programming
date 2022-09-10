577149a3247fe2a0c02b45ae76a5a0bda27c8e1e
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-10 02:19:28 +0200

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,19 +8,19 @@
  */
 int main(void)
 {	
-		int num = 0;
-			
-		while (num < 10)
+	int num = 0;
+		
+	while (num < 10)
+	{
+		putchar (num % 10 + '0');
+		if (num == 9)
 		{
-				putchar (num % 10 + '0');
-				if (num == 9)
-				{
-						break;
-				}
-				putchar(',');
-				putchar(' ');
-				num++;
+			break;
 		}
-		putchar('\n');
-		return (0);
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+	putchar('\n');
+	return (0);
 }
