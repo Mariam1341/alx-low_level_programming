d699cd0ed26c882f86ee8ca17b18568b23310c06
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-11 20:07:41 +0200

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i, j, k, l;
-	
+
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
@@ -20,18 +20,18 @@ int main(void)
 				{
 					if ((i == k && j < l) || (i < k))
 					{
-						if (i != k || j != l)
-						{
-							putchar(i);
-							putchar(j);
-							putchar(' ');
-							putchar(k);
-							putchar(l);
-							if (i == '9' && j == '8' && k == '9' && l == '9')
-								break;
-							putchar(',');
-							putchar(' ');
-						}
+					if (i != k || j != l)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+					if (i == '9' && j == '8' && k == '9' && l == '9')
+							break;
+					putchar(',');
+					putchar(' ');
+					}
 					}
 				}
 			}
