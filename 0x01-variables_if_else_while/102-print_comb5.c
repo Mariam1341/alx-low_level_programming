6ce4b14fd20db87d507e857b0b9f624a3cb937ab
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-11 20:02:35 +0200

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,36 +8,36 @@
  */
 int main(void)
 {
-int i, j, k, l;
-
-for(i = '0'; i <= '9'; i++)
-{
-	j = '0';
-	for(j = '0'; j <= '9'; j++)
+	int i, j, k, l;
+	
+	for(i = '0'; i <= '9'; i++)
 	{
-		for(k = '0'; k <= '9'; k++)
+		j = '0';
+		for(j = '0'; j <= '9'; j++)
 		{
-			for(l = '0'; l <= '9'; l++)
+			for(k = '0'; k <= '9'; k++)
 			{
-				if ((i == k && j < l) || (i < k))
+				for(l = '0'; l <= '9'; l++)
 				{
-					if (i != k || j != l)
+					if ((i == k && j < l) || (i < k))
 					{
-							putchar(i);
-							putchar(j);
-							putchar(' ');
-							putchar(k);
-							putchar(l);
-					if (i == '9' && j == '8' && k == '9' && l == '9')
-							break;
-					putchar(',');
-					putchar(' ');
+						if (i != k || j != l)
+						{
+								putchar(i);
+								putchar(j);
+								putchar(' ');
+								putchar(k);
+								putchar(l);
+						if (i == '9' && j == '8' && k == '9' && l == '9')
+								break;
+						putchar(',');
+						putchar(' ');
+						}
 					}
 				}
 			}
 		}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
