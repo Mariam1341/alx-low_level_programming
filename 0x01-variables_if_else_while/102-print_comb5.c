9b6bb8e15852c2c6517c71a29bcf2e57d9126e1c
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-11 20:05:27 +0200

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,34 +4,33 @@
  * main - main block
  * Description: Write a program that prints all possible
  * different combinations of two digits
- * Return: 0
+ * Return: 0s
  */
 int main(void)
 {
 	int i, j, k, l;
 	
-	for(i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		j = '0';
-		for(j = '0'; j <= '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			for(k = '0'; k <= '9'; k++)
+			for (k = '0'; k <= '9'; k++)
 			{
-				for(l = '0'; l <= '9'; l++)
+				for (l = '0'; l <= '9'; l++)
 				{
 					if ((i == k && j < l) || (i < k))
 					{
 						if (i != k || j != l)
 						{
-								putchar(i);
-								putchar(j);
-								putchar(' ');
-								putchar(k);
-								putchar(l);
-						if (i == '9' && j == '8' && k == '9' && l == '9')
+							putchar(i);
+							putchar(j);
+							putchar(' ');
+							putchar(k);
+							putchar(l);
+							if (i == '9' && j == '8' && k == '9' && l == '9')
 								break;
-						putchar(',');
-						putchar(' ');
+							putchar(',');
+							putchar(' ');
 						}
 					}
 				}
