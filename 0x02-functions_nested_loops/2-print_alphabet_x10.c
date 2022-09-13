33fb527971dacbe556a3eebe799d71c1eb2e1894
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-14 00:23:32 +0200

    prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ *Description: prints 10 times the alphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j = 0;
+
+	while (j < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
