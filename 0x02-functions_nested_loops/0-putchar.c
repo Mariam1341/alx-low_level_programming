96d0014a98dab674af2837fd81c0387715866153
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-13 19:37:21 +0200

    printf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *Description: Get a random number and print the number and
+ *if it is positive, negative, or zero
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
