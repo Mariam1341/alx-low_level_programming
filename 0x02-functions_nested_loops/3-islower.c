2e1d67f93603e13cdb8274691fcbc45f3260aead
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-14 00:35:30 +0200

    _

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ *Description: checks for lowercase character. if lower return 1 otherwise return 0
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if ( c < 123 && c > 96)
+	{
+		return 1;	
+	}
+	return 0;
+}
