18e5f5f8cd493d251fb52dc3724a3a1c464ff7ba
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-14 00:16:48 +0200

    betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,12 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void){
+void print_alphabet(void)
+{
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++){
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
-	}
+
 	_putchar('\n');
 }
