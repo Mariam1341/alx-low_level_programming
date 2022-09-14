16d140ba8b5a48d472d0c3d4bf1923bf7b53ac75
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-14 03:01:08 +0200

    parenthesis

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,6 +9,6 @@
 int _islower(int c)
 {
 	if (c < 123 && c > 96)
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,6 +9,6 @@
 int _isalpha(int c)
 {
 	if ( (c < 123 && c > 96) || (c < 91 && c > 64) )
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
