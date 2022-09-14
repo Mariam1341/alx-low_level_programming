5caa22e1e52bcac0c01efd808d4dc0160043d6e0
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-14 02:56:00 +0200

    check sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ *
+ *description:prints the sign of a number
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return 1;
+	}else if (n < 0)
+	{
+		_putchar('-');
+		return -1;
+	}else
+	{
+		_putchar('0');
+		return 0;
+	}
+}
