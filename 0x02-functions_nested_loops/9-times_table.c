3ab51056ce0dcdd5934d8a6d81a41d77812446d7
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-14 03:45:19 +0200

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,26 +8,26 @@
  */
 void times_table(void)
 {
-	int res;
+	int res, i, j;
 
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			res = i * j;
 			if (j == 0)
 			{
-				_putchar('0' + rep);
+				_putchar('0' + res);
 			}
 			else if (res < 10)
 			{
 				_putchar(' ');
-				_putchar('0' + rep);
+				_putchar('0' + res);
 			}
 			else
 			{
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
+				_putchar('0' + res / 10);
+				_putchar('0' + res % 10);
 			}
 
 			if (j < 9)
