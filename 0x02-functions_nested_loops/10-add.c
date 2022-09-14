896fe51b800baf6b28a8ce4c1d203230b7f4b803
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-14 03:43:40 +0200

    -

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add(int, int) - Entry point
+ *
+ *description:adds two integers and returns the result
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
  * jack_bauer(void) - Entry point
  *
- *description:prints every minute of the day, starting from 00:00 to 23:59.
+ *Description:prints every minute of the day, starting from 00:00 to 23:59.
  *
  * Return: Always 0 (Success)
  */
