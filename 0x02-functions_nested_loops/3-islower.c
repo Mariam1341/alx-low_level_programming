12b4993c969a3f8dba6d13ae86c7523b81642af0
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-14 02:51:10 +0200

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 /**
  * _islower - Entry point
  *
- *description:checks for lowercase character,if lower return 1 otherwise 0
+ *description:checks for lowercase character
  *
  * Return: Always 0 (Success)
  */
 int _islower(int c)
 {
 	if (c < 123 && c > 96)
-		return 1;	
+		return 1;
 	return 0;
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 /**
  * _isalpha - Entry point
  *
- *description: checks for alphabetic character.. if alphabet return 1 otherwise return 0
+ *description: checks for alphabetic character
  *
  * Return: Always 0 (Success)
  */
 int _isalpha(int c)
 {
-	if ( (c < 123 && c > 96) || (c < 91 && c > 64))
+	if ( (c < 123 && c > 96) || (c < 91 && c > 64) )
 		return 1;
 	return 0;
 }
