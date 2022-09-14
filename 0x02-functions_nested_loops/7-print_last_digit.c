3eb36640c6e46a3643f878177d6fc2a491ff73eb
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-14 03:21:16 +0200

    -

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit(int n) - Entry point
+ *
+ *description:prints the last digit of a number
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	return n % 10;	 
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * jack_bauer() - Entry point
+ *
+ *description: prints every minute of the day, starting from 00:00 to 23:59.
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+
+int h1;
+int h2;
+int m1;
+int m2;
+int a = 9;
+
+h2 = 0;
+while (h2 <= 2)
+{
+	if (h2 == 2)
+	{
+		/*Restrain to 23h, not 29*/
+		a = 3;
+	}
+	h1 = 0;
+	while (h1 <= a)
+	{
+		m2 = 0;
+		while (m2 <= 5)
+		{
+			m1 = 0;
+			while (m1 <= 9)
+			{
+				_putchar('0' + h2);
+				_putchar('0' + h1);
+				_putchar(':');
+				_putchar('0' + m2);
+				_putchar('0' + m1);
+				_putchar('\n');
+				m1++;
+			}
+			m2++;
+		}
+		h1++;
+	}
+	h2++;
+}
+
+}
