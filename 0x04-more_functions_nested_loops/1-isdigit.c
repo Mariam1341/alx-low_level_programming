02c97c1f8a7ac85c39b7f5179dc8cfc65d988c96
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-15 13:00:59 +0200

    -

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,11 @@
 /**
  * _isdigit -  checks for a digit (0 through 9)
  *
- *@c : character to check
+ *@c : character to check the case
  *
  * Return:0 or 1
  */
 int _isdigit(int c)
 {
-	if (c > 47 && c < 58)
-		return (1);
-	else
-		return (0);
+	return (c > 47 && c < 58);
 } 
