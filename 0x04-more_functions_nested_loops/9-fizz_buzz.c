a07f96c684f34fc65dbd49ff132c5ba66a91d15b
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-15 14:44:55 +0200

    fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf ("FizzBuzz ");
+		else
+		{
+			if ( i % 3 == 0)
+				printf ("Fizz ");
+			else if (i % 5 == 0)
+				printf ("Buzz ");
+			else
+				printf ("%i ", i);
+		}
+	}
+}
