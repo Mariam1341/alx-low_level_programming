6dcfc4ec5c550ef1a9fc2adf656c3a72843a7c70
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-15 13:25:53 +0200

    -

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,9 +2,6 @@
 /**
  * print_numbers -   prints the numbers, from 0 to 9
  *
- *@a : first number
- *@b : second number
- *
  * Return: multiplication of a and b
  */
 void print_numbers(void)
@@ -14,4 +11,4 @@ void print_numbers(void)
 	for (i = 0; i < 10; i++)
 		_putchar('0', i);
 	_putchar('\n');
-} 
+}
