8e97794f1ae02ebdbbed6346fb73b9f9a39db0a3
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-15 08:54:07 +0200

    -

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers -   prints the numbers, from 0 to 9
+ *
+ *@a : first number
+ *@b : second number
+ *
+ * Return: multiplication of a and b
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar('0', i);
+	_putchar('\n');
+} 
