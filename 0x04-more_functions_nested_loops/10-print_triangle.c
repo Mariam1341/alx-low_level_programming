3dadf97727b08186ef993ffce6b5d68498e93e8e
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-16 03:15:30 +0200

    -

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_triangle - prints a triangle, followed by a new line.
+*
+*@size : the size of the triangle
+*
+*Return : void
+*/
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -26,4 +26,5 @@ int main(void)
 			putchar(' ');
 	}
 	putchar('\n');
+	return 0;
 }
