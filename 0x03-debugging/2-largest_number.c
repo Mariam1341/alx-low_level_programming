ee0c63000cbd6d0c6bbedd49cdf0108386092fc4
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-19 17:59:58 +0200

    -

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,25 +10,25 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else if (c > b)
-	{
-		largest = c;
-	}
-	else
-	{
-		largest = b;
-	}
+if (a > b && a > c)
+{
+    largest = a;
+}
+else if (b > a && b > c)
+{
+    largest = b;
+}
+else if (c > b)
+{
+	largest = c;
+}
+else
+{
+	largest = b;
+}
 
-    return (largest);
+return (largest);
 }
 
