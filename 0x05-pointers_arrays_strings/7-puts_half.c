2d23e17080dfe4b4d29d7ea7837d74bcc805dc3a
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-19 19:20:42 +0200

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdio.h>
+/**
+* puts_half - prints half of a string, followed by a new line
+*
+*@str : pointer
+*
+*Return : void
+*/
+void puts_half(char *str)
+{
+	int i;
+	int len = strlen(str);
+
+	if (len % 2 == 1)
+		i = (len - 1) / 2;
+	else
+		i =  len / 2;
+	
+	while (i < len)
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
