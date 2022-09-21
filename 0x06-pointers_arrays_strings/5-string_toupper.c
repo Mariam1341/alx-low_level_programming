9c181099c0d0ed0799111ba17c2e389d4ac6f4bb
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-21 23:29:21 +0200

    changes to Upper Case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ *
+ * @a : pointer to the array
+ *
+ * Return : pointer to the result of concatenates two strings
+*/
+char *string_toupper(char *a)
+{
+	int i = 0, num;
+
+	while (a[i] != '\0')
+	{
+		num = (int) a[i];
+		if (num > 97)
+		{
+			num -= 32;
+			a[i] = (char) num;
+		}
+		i++;
+	}
+	return (a);
+}
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -6,47 +6,29 @@
  *
  * Return: Always 0.
  */
- void reverse_array(int *a, int n) //0 1 2 3 4 5 6 7 8 9
+char *string_toupper(char *a)
 {
-	int i = 0, j = n - 1, temp, half = n / 2;
-
-	while (i != j && i < half)
+	int i = 0, num;
+	
+	while (a[i] != '\0')
 	{
-		temp = a[i]; // 0 1 2 3 4
-		a[i] = a[j]; // 9 8 7 6 5
-		a[j] = temp; // 
-		i++; // 1 2 3 4 5 
-		j--; // 8 7 6 5 4
+		num = (int) a[i];
+		if (num > 97)
+		{
+			num -= 32;
+			a[i] = (char) num;
+		}
+		i++;
 	}
+	return (a);
 }
-void print_array(int *a, int n)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+    char str[] = "Look up!\n";
+    char *ptr;
 
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
     return (0);
 }
