6bc5f3ec7ca5c8f9f4033a1c5c68b6024dd9d977
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-21 20:48:17 +0200

    Betty ((:

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,10 +6,10 @@
  * @n: the number of characters from secound string
  *
  * Return: pointer to the result of concatenates two strings
-*/
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	int a = 0, i = 0;
+	int a = 0 int i = 0;
 
 	while (dest[i] != '\0')
 		i++;
@@ -22,6 +22,6 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	dest[i + 1] = '\0';
 
-	return (dest);	
+	return (dest);
 }
 
