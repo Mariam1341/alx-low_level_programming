b4c6983ba591635ef62cc99168a9d0506bc1efa1
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-22 18:54:50 +0200

    Betty ((:

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,17 +10,16 @@ char *leet(char *str)
   int i = 0, j;
   char cap[] = "aeotl", sm[] = "AEOTL";
   char n[] = "43071";
-s
+
   while (str[i] != '\0')
   {
-    j = 0;
-    while (cap[j] != '\0')
+    for (j = 0; j < 5; j ++)
     {
       if (str[i] == cap[j] || str[i] == sm[j])
       {
         str[i] = n[j];
+        break;
       }
-      j++;
     }
     i++;
   }
