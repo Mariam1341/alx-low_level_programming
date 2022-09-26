2afa7ee6cfca3a8ad15c3c4abf428301e2729d4a
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-26 22:41:30 +0200

    -

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -9,19 +9,7 @@
  * Return: Nothing.
  */
 
-void print_chessboard(char (*a)[8])
-{
-	int i, j;
-	
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			putchar(a[i][j]);
-		}
-		putchar('\n');
-	}
-}int main(void)
+int main(void)
 {
     char board[8][8] = {
         {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
