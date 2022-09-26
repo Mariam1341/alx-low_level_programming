f5e507619078b50986fde359c0bf9384edc76bd7
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-26 22:00:52 +0200

    -

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,12 +7,13 @@
 **/
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-	
-	for (i = 0; a[i][7]; i++)
+	int indx1, indx2;
+
+	for (indx1 = 0; a[indx1][7]; indx1++)
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n);
+		for (indx2 = 0; indx2 < 8; indx2++)
+			_putchar(a[indx1][indx2]);
+
+		_putchar('\n');
 	}
 }
