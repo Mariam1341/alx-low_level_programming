6424fa16e1caa3242e7abaa13616fb2f74d02679
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-26 20:58:43 +0200

    -

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,26 +8,17 @@
 **/
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j, pos, flag = 0;
-    for (i = 0; s[i] != '\0';i++);
-    pos = i;
-    for (i = 0; accept[i] != '\0'; i++)
-    {
-        for (j = 0; s[j] != '\0'; j++)
-        {
-            if (accept[i] == s[j])
-            {
-                if (j <= pos)
-                {
-                    pos = j;
-                    flag = 1;
-					break;
-                }
-            }
-         }
-    }
-    if (flag == 1)
-        return &s[pos];
-    else
-        return NULL;
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+
+		s++;
+	}
+	return ('\0');
 }
