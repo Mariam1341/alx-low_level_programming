8aa39c977acb3b327ba74c90dbb45a3f861a4496
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-29 02:07:13 +0200

    -

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
- * _puts_recursion : prints a string, followed by a new line
- *
- * @s: string to be printed.
- *
- * Return: Nothing
-**/
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to the string
+ * Return: void
+ */
 void _puts_recursion(char *s)
 {
 
