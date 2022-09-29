5500fa6b20497c3bfa59d963496b6323524e5524
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-29 04:25:07 +0200

    -

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+/**
+ * _sqrt_recursion - the natural square root of a number.
+ * @n: int
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1))
+}
+
+int square(int n, int val)
+{
+	if if (val * val < n)
+		return (square(n, val + 1));
+	else if (val * val == n)
+		return (i);
+	else
+		return (-1);
+}
+
