82b388b8252763caa7cad02c06cbb32a17b378aa
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-29 02:43:41 +0200

    -

--- 0x08-recursion/Untitled4.c
+++ 0x08-recursion/Untitled4.c
@@ -1,17 +1,12 @@
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		putchar('\n');
-	}
-	else
-	{
-		putchar(*s);
-		_puts_recursion(s + 1);
-	}
-}
+if (*s > '\0')
+{
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}}
 int main(void)
 {
-    _puts_recursion("Puts with recursion");
+    _print_rev_recursion("Puts with recursion");
     return (0);
 }
