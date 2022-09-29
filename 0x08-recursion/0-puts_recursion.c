57bf5265ba261e78c59310314213e8d23a7ddfe3
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-09-29 02:05:28 +0200

    -

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * _puts_recursion : prints a string, followed by a new line
  *
@@ -7,13 +8,15 @@
 **/
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		putchar('\n');
-	}
-	else
-	{
-		putchar(*s);
-		_puts_recursion(s + 1);
-	}
+
+if (*s == '\0')
+{
+	_putchar('\n');
+}
+else
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,4 +11,5 @@
 #include <stddef.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 #endif
