48e9c03ca5af46eb19aed562dc003ca300e1c599
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-10-04 00:13:54 +0200

    -

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%d\n", argc - 1);
+return (0);
+}
