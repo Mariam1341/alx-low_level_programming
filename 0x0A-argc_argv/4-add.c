b25181268c6cf196bdcf94f7976a18d461534596
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-10-04 21:31:35 +0200

    -

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * isInteger - checks if s is an integer
+ * @s: string to check
+ * Return: 0 or 1
+ */
+
+int isInteger(const char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+	if (s[i] < '0' || s[i] > '9')
+		return (1);
+	i++;
+}
+return (0);
+}
 
 /**
  * main - adds positive numbers
@@ -11,21 +28,14 @@
 int main(int argc, char const *argv[])
 {
 int sum = 0;
-int i = 1;
-
-if (argc == 1)
+while (--argc)
 {
-	printf("0\n");
-	return (0);	
-}
-while (i < argc)
-{
-	if (argv[i] < '0' || argv[i] > '9')
+	if (isInteger(argv[argc]))
 	{
 		printf("Error\n");
-		return (0);		
+		return (1);
 	}
-	sum += atoi(argv[i]);
+	sum += atoi(argv[argc]);
 }
 
 printf("%i\n", sum);
