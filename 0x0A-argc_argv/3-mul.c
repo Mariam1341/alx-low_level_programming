783c686fe9f0c3beb7d66a4a1f38e346a5d7fc29
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-10-04 21:06:12 +0200

    -

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%d\n", argv[1] * argv[2]);
+return (0);
+}
