efce1f2fae3bccde9a20018f4899b5752438c9c1
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-10-06 23:28:12 +0200

    -

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,6 +20,8 @@ char *str_concat(char *s1, char *s2)
 		for (j = 0; s2[j] != '\0'; j++);
 
 	s = malloc((i + j) * sizeof(*s1) + 1);
+	if (s == 0)
+		return (0);
 	if (i != 1)
 		for (i = 0; s1[i] != '\0'; i++)
 			s[i] = s1[i];
