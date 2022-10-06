64cbe951d6fc45cdff2851cd9279c5f324cdc646
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-10-06 23:23:15 +0200

    -

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,3 +1,12 @@
+#include <stdlib.h>
+/**
+* *str_concat - concatenates two strings.
+*
+* @s1 : 1st string
+* @s2 : 2nd string
+*
+* Return: pointer to a newly allocated space in memory or NULL
+**/
 char *str_concat(char *s1, char *s2)
 {
 	int i = 1, j = 1, k;
