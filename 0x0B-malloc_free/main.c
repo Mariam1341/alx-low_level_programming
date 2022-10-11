abea6ff283b7af41f45618382350ae3ee7da94e1
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-10-11 17:10:57 +0200

    -

--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -1,36 +1,57 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-char *argstostr(int ac, char **av)
+char **strtow(char *str)
 {
-	char *s;
-	int i = 0;
-
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	while (*av[i] != '\0')
+int i = 0, j = 0 , k = 0;
+char **strs;
+if (str == NULL || str == "")
+	return (NULL);
+for (; str[i] != '\0'; i++)
+{
+	if (str[i] != ' ')
 	{
-		s[i] = av[i] + "\n";
+		for (; str[i] != ' '; i++)
+		{
+			strs[j][k] = str[i];
+		}
+		i++;
+		j++;
+		k = 0;
 	}
-	return s;
-	
 }
+}
+/**
+ * print_tab - Prints an array of string
+ * @tab: The array to print
+ *
+ * Return: nothing
+ */
+void print_tab(char **tab)
+{
+    int i;
+
+    for (i = 0; tab[i] != NULL; ++i)
+    {
+        printf("%s\n", tab[i]);
+    }
+}
+
 /**
  * main - check the code for ALX School students.
  *
- * Return: Always 0.
+ * Return: 1 if an error occurred, 0 otherwise
  */
-int main(int ac, char *av[])
+int main(void)
 {
-    char *s;
+    char **tab;
 
-    s = argstostr(ac, av);
-    if (s == NULL)
+    tab = strtow("      ALX School         #cisfun      ");
+    if (tab == NULL)
     {
+        printf("Failed\n");
         return (1);
     }
-    printf("%s", s);
-    free(s);
+    print_tab(tab);
     return (0);
 }
