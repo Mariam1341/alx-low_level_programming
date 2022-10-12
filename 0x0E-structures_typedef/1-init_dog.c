ee2768eb535e2e82db0787dc7d50d6f38d32420f
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-10-12 02:00:56 +0200

    -

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,14 +7,14 @@
  * @name : string
  * @owner : string
  *
- * Return: void.
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+if (d != NULL)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,6 @@ void print_dog(struct dog *d)
 		printf("Name: %s\nAge: %f\nOwner: %s\n",
 		(*d).name == NULL ? d->name : "(nil)",
 		(*d).age,
-		d->owner == NULL ? d->owner : "(nil)",)
+		(*d).owner == NULL ? d->owner : "(nil)",)
 	}
 }
